860bc3868ce688d868bb12caa9e6b7b030e8ee9d
Willy Build <willy@build.local>
2026-07-19 04:50:09 +0700

    update: gitignore for kernel build artifacts

--- include/generated/compile.h
+++
@@ -1,4 +0,0 @@
-#define UTS_MACHINE		"aarch64"
-#define LINUX_COMPILE_BY	"willy"
-#define LINUX_COMPILE_HOST	"WIN-FVKTH9P31G2"
-#define LINUX_COMPILER		"aarch64-linux-gnu-gcc (Ubuntu 13.3.0-6ubuntu2~24.04.1) 13.3.0, GNU ld (GNU Binutils for Ubuntu) 2.42"
