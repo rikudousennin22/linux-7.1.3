928a892fb58020a7f1e836c55479ca931538e0c2
rikudousennin22 <116534193+rikudousennin22@users.noreply.github.com>
2026-07-19 05:05:21 +0700

    Delete scratch_patch.py

--- scratch_patch.py
+++
@@ -1,9 +0,0 @@
-import re
-
-kconfig_file = '/home/willy/linux-7.1.3/drivers/regulator/Kconfig'
-with open(kconfig_file, 'r') as f:
-    content = f.read()
-
-config_str = '''
-config REGULATOR_G22XX_CORE
-	tristate " GMT G22XX regulator core
